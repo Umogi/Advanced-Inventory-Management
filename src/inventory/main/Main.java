ef5db5f0024161428488b35cb274395d68a83c6c
kelly <152154777+Umogi@users.noreply.github.com>
2025-02-02 20:55:57 +0200

    Update Main.java

--- src/inventory/main/Main.java
+++ src/inventory/main/Main.java
@@ -1,41 +1,32 @@
 package inventory.main;
 
 import java.util.Scanner;
-import java.io.Console;
 
-import inventory.database.DatabaseConnection;
+import inventory.database.*;
 
 public class Main {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
         // Setting up resources
         Scanner scan = new Scanner(System.in);
-        Console console = System.console();
-
-        // Call the initialization method to run the SQL script
         DatabaseConnection.initializeDatabase();
-
-        // Application logic goes here
-        System.out.println("Inventory management system initialized.");
 
         while (true) {
             System.err.println("==== Log In Page ====");
             
             System.out.print("Username: ");
             String username = scan.nextLine();
-            
-            char[] passwordArray = console.readPassword("Password: ");
-            String password = new String(passwordArray);
+            System.out.print("Password: ");
+            String password = scan.nextLine();
 
             // Chechs User Credentials
-            
-            if (DatabaseConnection.checkUserCredentials(username, password) == true) {
-                System.out.println("Connection Succesful!");
+            if (Authentication.checkUserCredentials(username, password) == true) {
+                System.out.println("Connection Succesful!\n");
 
                 if (DatabaseConnection.checkUserType(username).equals("admin")) {
-                    
+                    AdminInteface.main(args);
                 }else if (DatabaseConnection.checkUserType(username).equals("user")) {
-                    
+                    UserInterface.main(args);
                 }else{
                     System.out.println("There was an error acured, please try again");
                 }
